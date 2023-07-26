72c8197d84a7c3eefcd4d1dcafdf184af4116994
ogenio <91586850+ogenio@users.noreply.github.com>
2023-07-26 03:36:34 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR CHECKUSER ANYVPN     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @LightXVD\033[1;36m"
+
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -16,15 +16,15 @@ pip3 install flask > /dev/null 2>&1
 rm /bin/chany > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/PhoenixxZ2023/checkany/main/chany > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/checkany/main/userscheck > /dev/null 2>&1
+wget https://raw.githubusercontent.com/ogenio/checkany/main/chany > /dev/null 2>&1
+wget https://raw.githubusercontent.com/ogenio/checkany/main/userscheck > /dev/null 2>&1
 chmod 777 chany > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /usr/lib/checkany > /dev/null 2>&1
 cd /usr/lib/checkany || exit
 rm checkany.py > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/checkany/main/checkany.py > /dev/null 2>&1
+wget https://raw.githubusercontent.com/ogenio/checkany/main/checkany.py > /dev/null 2>&1
 chmod 777 checkany.py > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
